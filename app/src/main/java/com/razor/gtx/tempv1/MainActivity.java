cf2550c9d6a5689e2be710f0c04842c0503b2081
skjha98 <skjha98@gmail.com>
2018-03-31 16:27:03 +0530

    Fifth Commit Branch RetrivalFirebase

--- app/src/main/java/com/razor/gtx/tempv1/MainActivity.java
+++ app/src/main/java/com/razor/gtx/tempv1/MainActivity.java
@@ -9,21 +9,30 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ListView list;
+
     private DatabaseReference databaseReference;
-    private EditText name, age, email, phone;
-    private TextView view;
-    private FloatingActionButton save;
+
+    private ArrayList<String> users = new ArrayList<>();
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,68 +40,40 @@ public class MainActivity extends AppCompatActivity {
 
         databaseReference = FirebaseDatabase.getInstance().getReference();
 
-        name = findViewById(R.id.name_editText);
-        age = findViewById(R.id.age_editText);
-        email = findViewById(R.id.email_editText);
-        phone = findViewById(R.id.phone_editText);
+        list = findViewById(R.id.list_listView);
 
-        view = findViewById(R.id.view_textView);
+        final ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, users);
+        list.setAdapter(adapter);
 
-        save = findViewById(R.id.done_fab);
 
-        save.setOnClickListener(new View.OnClickListener() {
+        databaseReference.addChildEventListener(new ChildEventListener() {
             @Override
-            public void onClick(View v) {
-                pushUser();
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                users.add(dataSnapshot.getValue(String.class));
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
             }
         });
 
-    }
-
-    @NonNull
-    private Boolean checkConnection(){
-
-        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
-        return activeNetwork != null && activeNetwork.isConnectedOrConnecting();
-    }
-
-    @NonNull
-    private User createUser(){
-
-        if(name.getText().toString().isEmpty() && age.getText().toString().isEmpty() && email.getText().toString().isEmpty() && phone.getText().toString().isEmpty()) {
-            Snackbar snackbar = Snackbar.make(findViewById(R.id.root_coordinatorLayout), "Please fill all above details", Snackbar.LENGTH_SHORT);
-            snackbar.show();
-        }
-
-        return new User(
-                name.getText().toString().trim(),
-                age.getText().toString().trim(),
-                email.getText().toString().trim(),
-                phone.getText().toString().trim()
-        );
-    }
-
-    private void pushUser(){
-
-        if (checkConnection()) {
-            databaseReference.push().setValue(createUser()).addOnCompleteListener(new OnCompleteListener<Void>() {
-                @Override
-                public void onComplete(@NonNull Task<Void> task) {
-                    if (task.isSuccessful()) {
-                        Snackbar snackbar = Snackbar.make(findViewById(R.id.root_coordinatorLayout), "Data Successfully Added", Snackbar.LENGTH_SHORT);
-                        snackbar.show();
-                        name.setText(null);
-                        age.setText(null);
-                        email.setText(null);
-                        phone.setText(null);
-                    }
-                }
-            });
-        } else {
-            Snackbar snackbar = Snackbar.make(findViewById(R.id.root_coordinatorLayout), "Turn ON Wi-Fi / Mobile Data", Snackbar.LENGTH_SHORT);
-            snackbar.show();
-        }
 
     }
 
